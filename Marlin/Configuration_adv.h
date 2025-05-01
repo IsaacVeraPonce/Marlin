a1c740779ee07723746225cd79060fe2db48c388
Isaac Vera Ponce <kiferingas@hotmail.com>
2025-05-01 14:32:53 -0600

    🔧 Update extruder auto fan temperature, adjust Z motor current, and enable CNC drilling cycle support

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -700,7 +700,7 @@
 #define CHAMBER_AUTO_FAN_PIN -1
 #define COOLER_AUTO_FAN_PIN -1
 
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 80
 #define EXTRUDER_AUTO_FAN_SPEED 255   // 255 == full speed
 #define CHAMBER_AUTO_FAN_TEMPERATURE 30
 #define CHAMBER_AUTO_FAN_SPEED 255
@@ -939,7 +939,7 @@
 //#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (linear=mm, rotational=°) Backoff from endstops after homing
 //#define XY_COUNTERPART_BACKOFF_MM 0         // (mm) Backoff X after homing Y, and vice-versa
 
-//#define QUICK_HOME                          // If G28 contains XY do a diagonal move first
+#define QUICK_HOME                            // If G28 contains XY do a diagonal move first
 //#define HOME_Y_BEFORE_X                     // If G28 contains XY home Y before X
 //#define HOME_Z_FIRST                        // Home Z first. Requires a real endstop (not a probe).
 //#define CODEPENDENT_XY_HOMING               // If X/Y can't home without homing Y/X first
@@ -2319,8 +2319,12 @@
       //#define BABYSTEP_DISPLAY_TOTAL          // Display total babysteps since last G28
     
       //#define BABYSTEP_ZPROBE_OFFSET          // Combine M851 Z and Babystepping
-      #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
-            //#define BABYSTEP_HOTEND_Z_OFFSET      // For multiple hotends, babystep relative Z offsets
+      //#define BABYSTEP_GLOBAL_Z               // Combine M424 Z and Babystepping
+    
+      #if ANY(BABYSTEP_ZPROBE_OFFSET, BABYSTEP_GLOBAL_Z)
+            #if ENABLED(BABYSTEP_ZPROBE_OFFSET)
+                  //#define BABYSTEP_HOTEND_Z_OFFSET    // For multiple hotends, babystep relative Z offsets
+            #endif
             //#define BABYSTEP_GFX_OVERLAY          // Enable graphical overlay on Z-offset editor
       #endif
 #endif
@@ -3064,7 +3068,7 @@
       #endif
     
       #if AXIS_IS_TMC_CONFIG(Z)
-            #define Z_CURRENT       800
+            #define Z_CURRENT       900
             #define Z_CURRENT_HOME  Z_CURRENT
             #define Z_MICROSTEPS     16
             #define Z_RSENSE          0.11
@@ -3900,6 +3904,14 @@
  */
 //#define CNC_COORDINATE_SYSTEMS
 
+/**
+ * CNC Drilling Cycle - UNDER DEVELOPMENT
+ *
+ * Enables G81 to perform a drilling cycle.
+ * Currently only supports a single cycle, no G-code chaining.
+ */
+//#define CNC_DRILLING_CYCLE
+
 // @section security
 
 /**
@@ -3995,6 +4007,15 @@
 #if ENABLED(FASTER_GCODE_PARSER)
       //#define GCODE_QUOTED_STRINGS  // Support for quoted string parameters
 #endif
+
+/**
+ * Variables
+ *
+ * Define a variable from 100-115 with G-code like '#101=19.6'.
+ * A variable can then be used in a G-code expression like 'G0 X[#101+3]'.
+ * See https://gcodetutor.com/cnc-macro-programming/cnc-variables.html
+ */
+//#define GCODE_VARIABLES
 
 /**
  * Support for MeatPack G-code compression (https://github.com/scottmudge/OctoPrint-MeatPack)
